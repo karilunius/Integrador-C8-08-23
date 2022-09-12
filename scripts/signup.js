f9406e2d54e2f343bb5e8b9cfca85b00a967098a
karilunius <93058053+karilunius@users.noreply.github.com>
2022-09-12 18:03:21 -0500

    001-signup-v001
    cambios en signup001

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,31 +1,30 @@
-window.addEventListener('load', function () {
-    /* ---------------------- obtenemos variables globales ---------------------- */
-   
+window.addEventListener("load", function () {
+	/* ---------------------- obtenemos variables globales ---------------------- */
+	const nombre = document.getElementById("inputNombre");
+	const apellido = document.getElementById("inputApellido");
+	const email = document.getElementById("inputEmail");
+	const password = document.getElementById("inputPassword");
+	const repetirPassword = document.getElementById("inputPasswordRepetida");
+	const form = document.querySelector("form");
 
+	/* -------------------------------------------------------------------------- */
+	/*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+	/* -------------------------------------------------------------------------- */
+	form.addEventListener("submit", function (event) {
+		event.preventDefault();
 
-    
+		const nuevoUsuario = {
+			nombre: nombre.value,
+			apellido: apellido.value,
+			email: email.value,
+            password: password.value,
+            repetirPassword: null,
+            
+		};
+	});
 
-    /* -------------------------------------------------------------------------- */
-    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
-    /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function (event) {
-      
-
-
-
-
-    });
-
-    /* -------------------------------------------------------------------------- */
-    /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
-    /* -------------------------------------------------------------------------- */
-    function realizarRegister(settings) {
-        
-
-
-
-
-    };
-
-
+	/* -------------------------------------------------------------------------- */
+	/*                    FUNCIÓN 2: Realizar el signup [POST]                    */
+	/* -------------------------------------------------------------------------- */
+	function realizarRegister(settings) {}
 });
