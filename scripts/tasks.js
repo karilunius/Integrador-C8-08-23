8497b87b16668ad3439d4e77af92b568cb2df555
Facuscar <facundo.scardillo@dracoder.com>
2022-09-16 19:21:36 -0300

    Renderizar tareas, cambiar estado de las mismas

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -13,6 +13,8 @@ window.addEventListener('load', function () {
   const btnCerrarSesion = document.querySelector('#closeApp');
   const formCrearTarea = document.querySelector('.nueva-tarea');
   const inputTarea = document.querySelector('#nuevaTarea');
+  const contenedorPendientes = document.querySelector('.tareas-pendientes')
+  const contenedorFinalizadas = document.querySelector('.tareas-terminadas')
   /* -------------------------------------------------------------------------- */
   /*                          FUNCIÓN 1 - Cerrar sesión                         */
   /* -------------------------------------------------------------------------- */
@@ -71,6 +73,8 @@ window.addEventListener('load', function () {
     fetch(URL, config).then( res => {return res.json()})
     .then( data => {
       console.log(data);
+      renderizarTareas(data);
+      botonesCambioEstado(data);
     })
   };
 
@@ -89,7 +93,7 @@ window.addEventListener('load', function () {
       description: nuevaTarea,
       completed: false,
     }
-    const URL = 'https://ctd-todo-api.herokuapp.com/v1/tasks'
+    const URL = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
     const config = {
       method: 'POST',
       headers: {
@@ -101,31 +105,101 @@ window.addEventListener('load', function () {
 
     fetch(URL, config).then( res => { return res.json()})
     .then(data => {
-      console.log(data);
+      renderizarTarea(data);
     })
   });
 
   /* -------------------------------------------------------------------------- */
   /*                  FUNCIÓN 5 - Renderizar tareas en pantalla                 */
   /* -------------------------------------------------------------------------- */
+  function renderizarTarea(tarea) {
+    const li = document.createElement('li');
+    li.classList.add('tarea');
+    if(tarea.completed) {
+      //tarea completada
+      li.innerHTML = `
+      <div class="hecha">
+
+        <i class="fa-regular fa-circle-check"></i>
+
+        </div>
+
+        <div class="descripcion">
+
+          <p class="nombre">${tarea.description}</p>
+
+          <div class="cambios-estados">
+
+          <button class="change completa" id="${tarea.id}" ><i class="fa-solid fa-rotate-left"></i></button>
+
+          <button class="borrar" id="${tarea.id}"><i class="fa-regular fa-trash-can"></i></button>
+
+        </div>
+
+      </div>
+      `
+      contenedorFinalizadas.appendChild(li);
+    } else {
+      //tarea pendiente
+      const fecha = new Date(tarea.createdAt);
+      li.innerHTML = `
+      <button class="change" id="${tarea.id}"><i class="fa-regular fa-circle"></i></button>
+
+      <div class="descripcion">
+
+        <p class="nombre">${tarea.description}</p>
+
+        <p class="timestamp">${fecha.toLocaleDateString()}</p>
+
+      </div>
+      `
+
+      contenedorPendientes.appendChild(li)
+    }
+  }
+
   function renderizarTareas(listado) {
 
-
-
-
-
-
-
+    listado.forEach( tarea => {
+      renderizarTarea(tarea);
+    })
+    
   };
 
   /* -------------------------------------------------------------------------- */
   /*                  FUNCIÓN 6 - Cambiar estado de tarea [PUT]                 */
   /* -------------------------------------------------------------------------- */
-  function botonesCambioEstado() {
-    
-    
+ 
 
+  function cambiarEstado(nodo, tarea) {
+    const URL = `https://ctd-todo-api.herokuapp.com/v1/tasks/${nodo.id}`
+    const data = {
+        completed: !tarea.completed,
+        description: tarea.description,
+    }
+    const config = {
+      method: 'PUT',
+      headers: {
+        authorization: jwt,
+        'Content-Type': "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(data),
+    }
+    fetch(URL,config).then( res => {return res.json()}).then(data => {
+      const { id } = data;
+      const nodo = document.getElementById(id).parentElement;
+      nodo.remove();
+      renderizarTarea(data)
+    })
+   
+  }
 
+  function botonesCambioEstado(listado) {
+    
+    const botones = document.querySelectorAll('.change');
+    botones.forEach( boton => {
+      boton.addEventListener('click', e => cambiarEstado(e.target, listado.find( tarea => tarea.id == e.target.id)))
+    })
 
   }
 
